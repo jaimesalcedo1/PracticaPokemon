3f9cc9b612eb2d9b2b1a36fff10de8a86abcaec8
Raul Rodriguez <raul.ma41544@cesurformacion.com>
2023-09-29 00:35:49 +0200

    Leer y escrbir csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         var pokemonController = PokemonController.getInstance();
+
+        PokemonController csv = new PokemonController();
+        csv.escribirCsv("datos.csv");
+        csv.leerCsv("datos.csv");
     }
 }
